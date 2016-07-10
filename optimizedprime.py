182aa535e093f7d4bf1f9f8b5f0b383b3bf683cc
Akshaya <akkshayya@gmail.com>
2016-07-10 18:32:20 +0530

    Add files via upload
    Program to find the sum of all the primes below two million.

---
+++ optimizedprime.py
@@ -0,0 +1,27 @@
+'''The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17. Find the sum of all
+the primes below two million.
+'''
+
+def prime():
+	# number to find summation of all primes below number
+	no = 2000000
+
+	# create a set excluding even numbers
+	numbers = set(xrange(3, no + 1, 2)) 
+
+	for number in xrange(3, int(no ** 0.5) + 1):
+	    if number not in numbers:
+		#number must have been removed because it has a prime factor
+		continue
+
+	    num = number
+	    while num < no:
+		num += number
+		if num in numbers:
+		    # Remove multiples of prime found
+		    numbers.remove(num)
+
+	print 2 + sum(numbers)
+
+if __name__ == '__main__':
+	prime()
